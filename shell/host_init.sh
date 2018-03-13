f791a2154adc86cfe1c751208be366dc7ab61606
mds1455975151 <1455975151@qq.com>
2018-03-13 22:01:33 +0800

    add file

--- shell/host_init.sh
+++ shell/host_init.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 # host init scripts
 
+base_dir="/data0/src"
+
 yum_setup(){
 yum install -y wget
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
@@ -8,7 +10,7 @@ wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 }
 
 packages_install(){
-yum install -y gcc gcc-c++ autoconf automake python-devel python-pip git tree nmap telnet bash-completion net-tools bind-utils tcpdump lsof vim wget
+yum install -y gcc gcc-c++ autoconf automake python-devel python-pip git tree nmap telnet bash-completion net-tools bind-utils tcpdump lsof vim screen
 }
 
 pip_setup(){
@@ -23,8 +25,8 @@ pip install pip --upgrade
 }
 
 sublist3r_install(){
-mkdir -p /data0/src
-cd /data0/src
+mkdir -p ${base_dir}
+cd ${base_dir}
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r/
 pip install -r requirements.txt
