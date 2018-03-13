6c2e668e13ca94f4f8654689480aeda886abe9f0
mds1455975151 <1455975151@qq.com>
2018-03-13 15:36:45 +0800

    change file

--- shell/host_init.sh
+++ shell/host_init.sh
@@ -6,6 +6,7 @@ yum install -y wget
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 }
+
 packages_install(){
 yum install -y gcc gcc-c++ autoconf automake python-devel python-pip git tree nmap telnet bash-completion net-tools bind-utils tcpdump lsof vim wget
 }
@@ -29,10 +30,22 @@ cd Sublist3r/
 pip install -r requirements.txt
 }
 
+service_setup(){
+systemctl stop NetworkManager
+systemctl disable NetworkManager
+}
+
+git_setup(){
+git config --global user.name "mds1455975151"
+git config --global user.email 1455975151@qq.com
+}
+
 main(){
 yum_setup
 packages_install
 pip_setup
+service_setup
+git_setup
 }
 
 main
