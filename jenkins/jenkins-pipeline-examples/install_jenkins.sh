248dff98629cc3cbe4e2ba33d781303a933aeb79
mds1455975151 <1455975151@qq.com>
2018-03-15 21:24:46 +0800

    add file

--- jenkins/jenkins-pipeline-examples/install_jenkins.sh
+++ jenkins/jenkins-pipeline-examples/install_jenkins.sh
@@ -1,5 +1,6 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install -y java-1.8.0-openjdk
 sudo yum install -y jenkins
 sudo service jenkins restart
 sleep 10
