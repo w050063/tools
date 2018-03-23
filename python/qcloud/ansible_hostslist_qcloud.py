c0de9ba952883d396ff25b9d5dde440167bfdadb
mds1455975151 <1455975151@qq.com>
2018-03-23 17:32:54 +0800

    add file

---
+++ python/qcloud/ansible_hostslist_qcloud.py
@@ -0,0 +1,39 @@
+from QcloudApi.qcloudapi import QcloudApi
+
+import json
+import yaml
+import sys
+
+conf_file="api.yaml"
+module = 'cvm'
+action = 'DescribeInstances'
+
+def read_config(conf_file):
+    f = open(conf_file)
+    conf_yaml = yaml.load(f)
+    config = conf_yaml
+    return config
+
+def request_put(module,config,action,params):
+    service = QcloudApi(module, config)
+    str_results = service.call(action, params)
+    json_results = json.loads(str_results)
+    return json_results
+
+
+def main():
+    config = read_config(conf_file)
+    params = {'Limit':100}
+    json_results = request_put(module,config,action,params)
+    for i in json_results['Response']['InstanceSet']:
+        name = i['InstanceName']
+        wanip = i['PublicIpAddresses'][0]
+        lanip = i['PrivateIpAddresses'][0]
+        os = i['OsName']
+        cpu = i['CPU']
+        mem = i['Memory']
+        info = "%s %s qq" % (wanip,name,)
+        print info
+
+if __name__ == '__main__':
+    sys.exit(main())
--- python/qcloud/qcloud.py
+++ python/qcloud/qcloud.py
@@ -2,31 +2,58 @@ from QcloudApi.qcloudapi import QcloudApi
 
 import json
 import yaml
+import sys
 
 conf_file="api.yaml"
 
-f = open(conf_file)
-conf_yaml = yaml.load(f)
-print conf_yaml
-Region = conf_yaml['Region']
-secretId = conf_yaml['secretId']
-secretKey = conf_yaml['secretKey']
-Version = conf_yaml['Version']
+class MyApi:
 
-module = 'cvm'
-action = 'DescribeInstances'
-config = {'Region':Region, 'secretId':secretId, 'secretKey':secretKey, 'Version':Version}
-params = {'Limit':1}
-service = QcloudApi(module, config)
-str_results = service.call(action, params)
-json_results = json.loads(str_results)
-print json_results
-for i in json_results['Response']['InstanceSet']:
-    name = i['InstanceName']
-    wanip = i['PublicIpAddresses'][0]
-    lanip = i['PrivateIpAddresses'][0]
-    os = i['OsName']
-    cpu = i['CPU']
-    mem = i['Memory']
-    info = "name=%s\twanip=%s\tlanip=%s\tconf=%score*%sG\tos=%s" % (name,wanip,lanip,cpu,mem,os)
-    print info
+    def __init__(self, config,module,action,params):
+	self.config = config
+	self.module = module
+	self.action = action
+	self.params = params
+
+    def request_put(self):
+        service = QcloudApi(self.module, self.config)
+        str_results = service.call(self.action, self.params)
+        json_results = json.loads(str_results)
+        return json_results
+
+    def cvm_list(self):
+	json_results = self.request_put()
+	print json_results
+	for i in json_results['Response']['InstanceSet']:
+		name = i['InstanceName']
+		wanip = i['PublicIpAddresses'][0]
+		lanip = i['PrivateIpAddresses'][0]
+		os = i['OsName']
+		cpu = i['CPU']
+		mem = i['Memory']
+		info = "type=%s\tname=%s\t\twanip=%s\tlanip=%s\tconf=%score*%sG\tos=%s" % (module,name,wanip,lanip,cpu,mem,os)
+		print info
+
+    def lb_list(self):
+        json_results = self.request_put()
+        return json_results
+
+def read_config(conf_file):
+    f = open(conf_file)
+    conf_yaml = yaml.load(f)
+    config = conf_yaml
+    return config
+
+
+def main():
+    config = read_config(conf_file)
+    params = {'Limit':100}
+    module = 'lb'
+    action = 'DescribeLoadBalancers'
+    s1 = MyApi(config,module,action,params)
+    print s1.module
+    a = s1.lb_list()
+    print a
+
+if __name__ == '__main__':
+    sys.exit(main())
+
