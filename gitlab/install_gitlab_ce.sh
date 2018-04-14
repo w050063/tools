a064183e882f3f3c94dab274703fa388c7aea76d
mds1455975151 <1455975151@qq.com>
2018-04-14 22:47:52 +0800

    add gitlab

--- gitlab/install_gitlab_ce.sh
+++ gitlab/install_gitlab_ce.sh
@@ -2,6 +2,7 @@
 
 data_dir="/data0/src"
 app_version="gitlab-ce-10.6.4-ce.0.el7.x86_64.rpm"
+ip="192.168.200.102"
 
 [ ! -d ${data_dir} ] && mkdir -p ${data_dir}
 
@@ -20,10 +21,12 @@ sudo systemctl start postfix
 cat>/etc/yum.repos.d/gitlab-ce.repo<<EOF
 [gitlab-ce]
 name=Gitlab CE Repository
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el$releasever/
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el7/
 gpgcheck=0
 enabled=1
 EOF
 
 sudo yum makecache
-sudo EXTERNAL_URL="http://192.168.200.102" yum install -y gitlab-ce
+yum install -y gitlab-ce
+sed -i "s#external_url 'http://gitlab.example.com'#external_url 'http://${ip}'#g" /etc/gitlab/gitlab.rb
+gitlab-ctl reconfigure
