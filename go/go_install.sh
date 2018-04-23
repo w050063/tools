d5e732abda30d057f8d6fb69ff8be1a8a57cfae9
mds1455975151 <1455975151@qq.com>
2018-04-23 22:29:29 +0800

    add file

--- go/go_install.sh
+++ go/go_install.sh
@@ -2,7 +2,8 @@
 
 wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
 tar -zxf go1.10.1.linux-amd64.tar.gz -C /usr/local/
-cat>>/etc/profile<<EOF
+grep -q GOPATH /etc/profile
+[ ! $? -eq 0 ] && cat>>/etc/profile<<EOF
 
 # setup go path env
 export PATH=\$PATH:/usr/local/go/bin
