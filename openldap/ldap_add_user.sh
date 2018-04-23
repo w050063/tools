b6e45965ddc3ecd8a6662479b52ccb1bec8f2c31
mds1455975151 <1455975151@qq.com>
2018-04-23 17:55:33 +0800

    Update ldap_add_user.sh

--- openldap/ldap_add_user.sh
+++ openldap/ldap_add_user.sh
@@ -1,17 +1,40 @@
 #!/bin/env bash
 
-if [ ! $# -eq 1 ]
+if [ ! $# -eq 3 ]
 then
-    echo -e "Usages: $0 ldap_username"
+    echo -e "Usages: $0 ldap_username ldap_uid ldap_gid"
+    echo -e """
+manager        1002
+planner        1003
+programmer     1004
+qa             1005
+art            1006
+operator       1007
+marketer       1008
+financial      1009
+administration 1010
+"""
     exit 1
 fi
 
 ldap_username=$1
+ldap_uid=$2
+ldap_gid=$3
+
+groupadd -g 1002 -f manager
+groupadd -g 1003 -f planner
+groupadd -g 1004 -f programmer
+groupadd -g 1005 -f qa
+groupadd -g 1006 -f art
+groupadd -g 1007 -f operator
+groupadd -g 1008 -f marketer
+groupadd -g 1009 -f financial
+groupadd -g 1010 -f administration 
 
 grep -q $ldap_username /etc/passwd
 if [ ! $? -eq 0 ]
 then
-    useradd $ldap_username
+    useradd -u ${ldap_uid} -g ${ldap_gid} $ldap_username
     echo "$ldap_username" | passwd --stdin $ldap_username
 else
     echo "$ldap_username is exist!!!"   
