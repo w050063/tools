5ff7cbf94635f10ca15e26f3ea714388229f48bd
mds1455975151 <1455975151@qq.com>
2018-04-24 22:46:32 +0800

    Update svn_httpd_ldap.sh

--- svn/svn_httpd_ldap.sh
+++ svn/svn_httpd_ldap.sh
@@ -7,7 +7,11 @@
 #!/bin/env bash
 yum install -y httpd svn mod_dav_svn mod_ldap
 cp /etc/httpd/conf.modules.d/10-subversion.conf /etc/httpd/conf.d/subversion.conf
-cat>>/etc/httpd/conf.d/subversion.conf<<EOF
+cat>/etc/httpd/conf.d/subversion.conf<<EOF
+LoadModule dav_svn_module     modules/mod_dav_svn.so
+LoadModule authz_svn_module   modules/mod_authz_svn.so
+LoadModule dontdothat_module  modules/mod_dontdothat.so
+
 <Location /opt/svn>
     DAV svn
     SVNParentPath /opt/svn
@@ -32,5 +36,44 @@ chown apache.apache -R /opt/svn
 
 # svn + httpd
 ``` bash
+#!/bin/env bash
+yum install -y httpd svn mod_dav_svn
+cp /etc/httpd/conf.modules.d/10-subversion.conf /etc/httpd/conf.d/subversion.conf
+
+cat>/etc/httpd/conf.d/subversion.conf<<EOF 
+LoadModule dav_svn_module     modules/mod_dav_svn.so
+LoadModule authz_svn_module   modules/mod_authz_svn.so
+LoadModule dontdothat_module  modules/mod_dontdothat.so
+
+<Location /opt/svn>
+    DAV svn
+    SVNParentPath /opt/svn
+
+    AuthType Basic
+    AuthName "Subversion repository"
+    AuthzSVNAccessFile /opt/svn/test/authz
+    AuthUserFile /opt/svn/test/password
+    Require valid-user
+</Location>
+
+systemctl start httpd.service
+systemctl enable httpd.service
+cd /opt/svn && svnadmin create test
+svnserve -d -r /opt/svn/
+chown apache.apache -R /opt/svn
+
+touch /opt/svn/test/conf/httpd_passwd  #创建用户文件
+
+htpasswd /opt/svn/test/conf/httpd_passwd admin  #创建用户admin
+htpasswd /opt/svn/test/conf/httpd_passwd guest  #创建用户guest
+
+# cat /opt/svn/test/conf/httpd_passwd 
+admin:$apr1$UCkPzZ2x$tnDk2rgZoiaURPzO2e57t0
+guest:$apr1$vX1RIUq6$OKS1bqKZSptzsPDYUOJ5x.
+
+# cat /opt/svn/test/conf/authz
+[/]
+admin = rw
+guest = r
 
 ```
