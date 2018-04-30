12659eb1309b80203aa066986895a95b0d37e423
mds1455975151 <1455975151@qq.com>
2018-04-30 18:30:28 +0800

    Update install_twemproxy.sh

--- twemproxy/install_twemproxy.sh
+++ twemproxy/install_twemproxy.sh
@@ -5,7 +5,9 @@ git clone https://github.com/twitter/twemproxy.git
 cd twemproxy/
 git checkout -b v0.4.1
 git branch -a
+CFLAGS="-ggdb3 -O0"
 autoreconf -fvi
-./configure --enable-debug=full
+./configure --enable-debug=log
+# ./configure --enable-debug=full
 make
 src/nutcracker -h
