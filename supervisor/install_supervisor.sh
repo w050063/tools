65faa11884f2022e90f57015220e4f6f10654c8e
mds1455975151 <1455975151@qq.com>
2018-04-30 18:44:49 +0800

    Update install_supervisor.sh

--- supervisor/install_supervisor.sh
+++ supervisor/install_supervisor.sh
@@ -1,9 +1,24 @@
 #!/bin/env bash
 
+pip_install(){
 yum install -y python-pip
 pip install supervisor
-mkdir -p /etc/supervisor/conf.d/
-echo_supervisord_conf >/etc/supervisor/supervisord.conf
-sed -i 's#;[include]#[include]#g' /etc/supervisor/supervisord.conf
-sed -i 's#;files = relative/directory/*.ini#files = /etc/supervisor/conf.d/*.ini#g' /etc/supervisor/supervisord.conf
-supervisord -c /etc/supervisor/supervisord.conf
+mkdir -p /etc/supervisord.d
+echo_supervisord_conf >/etc/supervisord.conf
+sed -i 's#;[include]#[include]#g' /etc/supervisord.conf
+sed -i 's#;files = relative/directory/*.ini#files = /etc/supervisord.d/*.ini#g' /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf
+}
+
+yum_install(){
+yum install -y supervisord
+systemctl start supervisord.service 
+systemctl enable supervisord.service
+}
+
+main(){
+# pip_install
+yum_install
+}
+
+main
