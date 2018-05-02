630df6cd5b55dce09f71a7b1e3a8ffc6d3c27755
mds1455975151 <1455975151@qq.com>
2018-05-02 09:54:41 +0800

    Update install_newrelic.sh

--- php/install_newrelic.sh
+++ php/install_newrelic.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 
-license-key="cfc8926965a3111ec0b28e5a69f114d657a40074"
-newrelic.appname=`echo `date +%Y%m%d%H%M%S`|md5sum|awk '{print $1}'`
+time_flag=`date +%Y%m%d%H%M%S`
+license_key="cfc8926965a3111ec0b28e5a69f114d657a40074"
+newrelic_appname=`echo ${time_flag}|md5sum|awk '{print $1}'`
 
 install_newrelic(){
 rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
@@ -12,9 +13,9 @@ cp /etc/php.d/newrelic.ini{,.`date +%Y%m%d`}
 cat >/etc/php.d/newrelic.ini<<EOF
 extension = "newrelic.so"
 [newrelic]
-newrelic.license = "${license-key}"
+newrelic.license = "${license_key}"
 newrelic.logfile = "/var/log/newrelic/php_agent.log"
-newrelic.appname = "${newrelic.appname}"
+newrelic.appname = "${newrelic_appname}"
 newrelic.daemon.logfile = "/var/log/newrelic/newrelic-daemon.log"
 newrelic.daemon.port = "@newrelic-daemon"
 newrelic.transaction_tracer.threshold = "10ms"
