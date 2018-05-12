819d56407c8091a143497e4a53b1b7379b20ae89
mds1455975151 <1455975151@qq.com>
2018-05-12 21:38:00 +0800

    Update install_aria2.sh

--- aria2/install_aria2.sh
+++ aria2/install_aria2.sh
@@ -9,9 +9,10 @@ make && make install
 
 aria2c --enable-rpc --rpc-listen-all --rpc-allow-origin-all -c  --dir /root/downloads -D
 
-git clone https://github.com/ziahamza/webui-aria2
-cd webui-aria2/
-python -m SimpleHTTPServer 9999 
+mkdir -p /data0/
+cd /data0/ && git clone https://github.com/ziahamza/webui-aria2
+cd webui-aria2/ && python -m SimpleHTTPServer 9999
+mkdir webui-aria2/logs
 }
 
 main(){
