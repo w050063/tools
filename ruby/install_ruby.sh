364a9f97478467115f3b766f2a7b23d230d1e4de
mds1455975151 <1455975151@qq.com>
2018-05-19 16:13:08 +0800

    add file

---
+++ ruby/install_ruby.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+install_ruby(){
+ruby --version
+wget https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.1.tar.gz
+tar -zxf ruby-2.5.1.tar.gz
+cd ruby-2.5.1
+./configure
+make && make install
+yum install -y gem
+gem sources --add http://gems.ruby-china.org/ --remove https://rubygems.org/(http)
+#gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/(https)
+}
+
+main(){
+install_ruby
+}
+
+main
