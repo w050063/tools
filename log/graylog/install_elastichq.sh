bc02a48e1e0108a62330df7e6c3d61c48cfa252c
mds1455975151 <1455975151@qq.com>
2018-06-18 09:58:26 +0800

    move graylog to log

--- log/graylog/install_elastichq.sh
+++ log/graylog/install_elastichq.sh
@@ -1,21 +1,17 @@
-# ElasticHQ
-- 官网地址:http://www.elastichq.org/index.html
-- 官网文档:http://docs.elastichq.org/
-- GitHub:https://github.com/ElasticHQ/elasticsearch-HQ
+#!/bin/env bash
 
-``` bash
 yum install -y python34
 python3.4 -V
+mkdir -p /data0/src/
 cd /data0/ && git clone https://github.com/ElasticHQ/elasticsearch-HQ
 cd elasticsearch-HQ/
 python3.4 install -r requirements.txt
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.4 get-pip.py
 pip3 install -r requirements.txt
-python3.4 application.py
-nohup python3.4 application.py &
-or
-wget https://raw.githubusercontent.com/mds1455975151/tools/master/supervisor/install_supervisor.sh
+#python3.4 application.py
+#nohup python3.4 application.py &
+cd /data0/src/ && wget https://raw.githubusercontent.com/mds1455975151/tools/master/supervisor/install_supervisor.sh
 sh install_supervisor.sh
 
 cat>/etc/supervisord.d/ElasticHQ.ini<<EOF
@@ -30,4 +26,3 @@ stderr_logfile=/var/log/ElasticHQ_err.log
 stdout_logfile=/var/log/ElasticHQ_out.log
 EOF
 systemctl restart supervisord.service
-```
