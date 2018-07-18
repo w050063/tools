6661ac8456cf8796ae83688c982d57f14ed091b8
mds1455975151 <1455975151@qq.com>
2018-07-18 15:15:34 +0800

    Update install_supervisor.sh

--- supervisor/install_supervisor.sh
+++ supervisor/install_supervisor.sh
@@ -7,6 +7,16 @@ mkdir -p /etc/supervisord.d
 echo_supervisord_conf >/etc/supervisord.conf
 sed -i 's#;[include]#[include]#g' /etc/supervisord.conf
 sed -i 's#;files = relative/directory/*.ini#files = /etc/supervisord.d/*.ini#g' /etc/supervisord.conf
+
+grep -q 09348c20a019be0318387c08df7a783d /etc/supervisord.conf
+[ $? ! -eq 0 ] && cat>>/etc/supervisord.conf<<EOF
+
+[inet_http_server]
+port=0.0.0.0:9001
+username=supervisor
+password=09348c20a019be0318387c08df7a783d
+EOF
+
 supervisord -c /etc/supervisord.conf
 }
 
