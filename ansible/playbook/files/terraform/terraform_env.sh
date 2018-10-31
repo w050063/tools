0b6b72a5588102c7b7d40790b02521f12ff472df
mds1455975151 <1455975151@qq.com>
2018-10-31 21:11:35 +0800

    add files

---
+++ ansible/playbook/files/terraform/terraform_env.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+sed -i '/terraform bin PATH setup/d' /etc/profile
+sed -i '/\/usr\/local\/terraform\//d' /etc/profile
+
+cat>>/etc/profile<<EOF
+
+# terraform bin PATH setup
+export PATH=\$PATH:/usr/local/terraform/
+EOF
+source /etc/profile
+
